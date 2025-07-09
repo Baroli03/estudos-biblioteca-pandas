08f71cacf8a739c6c88150264fe67320316b042a
Eduardo Baroli Souza <eduardo_Baroli@hotmail.com>
2025-07-09 10:52:35 -0300

    feat: Adicionando atividade de filtragem de apartamentos com 1 quarto até 1200 reais

---
+++ filtragem_dados/aptos-1q-ate-1200.py
@@ -0,0 +1,14 @@
+from dados.dados import df_apartamentos
+
+# Objetivo: Filtrar Apartamentos que possuem 1 Quarto e Aluguel menor que 1200
+
+# Minha resposta para a atividade:
+df_1q_ate_1200 = df_apartamentos.query('Quartos == 1 and Valor < 1200')
+# print(df_1q_ate_1200.head())
+# Outra aprendida no curso:
+selecao1 = df_apartamentos['Quartos'] == 1
+df_1q_ate_1200_forma2 = df_apartamentos[selecao1]
+selecao2 = df_apartamentos['Valor'] < 1200
+selecao_final = (selecao1) & (selecao2)
+df_1q_ate_1200_forma2 = df_apartamentos[selecao_final]
+print(df_1q_ate_1200_forma2)
